6e47b82185849c51df128e9bfa675b1218f17d6d
Aaronong <aaronongcs@gmail.com>
2017-04-02 06:09:42 +0800

    first bug free edition

--- authentication/views.py
+++ authentication/views.py
@@ -3,18 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 from stocks.models import ParentOrder, ChildOrder
 
-@login_required(login_url="login/")
+
 def index(request):
-    order_list = ParentOrder.objects.all().order_by('-id')
-    for order in order_list:
-        children = ChildOrder.objects.filter(parent_order__id=order.id)
-        # get stats
-        order_stats = order.get_stats(children)
-        # format stats
-        order.avg_price = '{:,.2f}'.format(order_stats['average_price'])
-        order.progress = '{:,.2f}'.format(order.progress)
-
-    context_dict = {'orders': order_list}
-
-    # render homepage
-    return render(request, "index.html", context_dict)
+    return render(request, 'tags.html')
--- notetaker/urls.py
+++ notetaker/urls.py
@@ -1,11 +1,16 @@
 # authentication/urls.py
 from django.conf.urls import url
 from notetaker import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from authentication.forms import LoginForm
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^notebooks$', views.notebooks, name='notebooks'),
-    url(r'^documents$', views.documents, name='documents'),
-    url(r'^tags$', views.tags, name='tags'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
+    url(r'^notebooks/$', views.notebooks, name='notebooks'),
+    url(r'^documents/$', views.documents, name='documents'),
+    url(r'^tags/$', views.tags, name='tags'),
     url(r'^(?P<note_id>[0-9]+)/$', views.noteedit, name='NoteEdit'),
 ]
